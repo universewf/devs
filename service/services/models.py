c3fd867674039af7c25001d2cd8c0b9f7d58200f
Garik Baghdasaryan <mcclean449@gmail.com>
2025-03-19 22:11:39 +0300

    recount price/discount

--- service/services/models.py
+++ service/services/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
 from clients.models import Client
+from services.tasks import set_price
 
 # Create your models here.
 class Service(models.Model):
     name = models.CharField(max_length=100)
     full_price = models.PositiveIntegerField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__full_price = self.full_price
+
+    def save(self, *args,**kwargs):
+        if self.full_price != self.__full_price:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+        return super().save(*args,**kwargs)
+
 
     def __str__(self):
         return self.name
@@ -23,8 +35,21 @@ class Plan(models.Model):
     def __str__(self):
         return self.plan_type
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__discount_percent = self.discount_percent
+
+    def save(self, *args,**kwargs):
+        if self.discount_percent != self.__discount_percent:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+        return super().save(*args,**kwargs)
+
 
 class Subscription(models.Model):
     client = models.ForeignKey(to=Client,related_name='subscriptions',on_delete=models.PROTECT)
     service = models.ForeignKey(to=Service,related_name='subscriptions',on_delete=models.PROTECT)
     plan = models.ForeignKey(to=Plan,related_name='subscriptions',on_delete=models.PROTECT)
+    price  = models.PositiveIntegerField(default=0)
+
+
--- service/services/views.py
+++ service/services/views.py
@@ -9,10 +9,7 @@ from django.db.models import Sum
 
 # Create your views here.
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all().prefetch_related('plan',Prefetch('client',queryset=Client.objects.all().select_related('user').only('company_name','user__email'))).annotate(
-        price=F('service__full_price')-
-        F('service__full_price')*
-        F('plan__discount_percent')/100.00)
+    queryset = Subscription.objects.all().prefetch_related('plan',Prefetch('client',queryset=Client.objects.all().select_related('user').only('company_name','user__email')))
     serializer_class = SubscriptionSerializer
 
 
